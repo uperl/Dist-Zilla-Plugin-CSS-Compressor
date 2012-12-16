bb8e02230ac38ccb73d794405f3fb52fe3843d9f
Graham Ollis <perl@wdlabs.com>
2012-12-16 18:55:41 -0500

    doco

--- lib/Dist/Zilla/Plugin/CSS/Compressor.pm
+++ lib/Dist/Zilla/Plugin/CSS/Compressor.pm
@@ -8,15 +8,56 @@ use Dist::Zilla::File::FromCode;
 # ABSTRACT: Compress CSS files
 # VERSION
 
+=head1 SYNOPSIS
+
+ [CSS::Compressor]
+
+=head1 DESCRIPTION
+
+Compress CSS files in your distribution using L<CSS::Compressor>.  By default for
+each C<foo.css> file in your distribution this plugin will create a C<foo.min.css>
+which has been compressed.
+
+=cut
+
 with 'Dist::Zilla::Role::FileGatherer';
 with 'Dist::Zilla::Role::FileInjector';
 
 use namespace::autoclean;
 
+=head1 ATTRIBUTES
+
+=head2 finder
+
+Specifies a L<FileFinder|Dist::Zilla::Role::FileFinder> for the CSS files that
+you want compressed.  If this is not specified, it will compress all the CSS
+files that do not have a C<.min.> in their filenames.  Roughly equivalent to
+this:
+
+ [FileFinder::ByName / CSSFiles]
+ file = *.css
+ skip = .min.
+ [CSS::Compressor]
+ finder = CSSFile
+
+=cut
+
 has finder => (
   is  => 'ro',
   isa => 'Str',
 );
+
+=head2 output_regex
+
+Regular expression substitution used to generate the output filenames.  By default
+this is
+
+ [CSS::Compressor]
+ output_regex = /\.css$/.min.css/
+
+which generates a C<foo.min.css> for each C<foo.css>.
+
+=cut
 
 has output_regex => (
   is      => 'ro',
@@ -24,10 +65,25 @@ has output_regex => (
   default => '/\.css$/.min.css/',
 );
 
+=head2 output
+
+Output filename.  Not used by default, but if specified, all CSS files are merged and
+compressed into a single file using this as the output filename.
+
+=cut
+
 has output => (
   is  => 'ro',
   isa => 'Str',
 );
+
+=head1 METHODS
+
+=head2 $plugin-E<gt>gather_files( $arg )
+
+This method adds the compressed CSS files to your distribution.
+
+=cut
 
 sub gather_files
 {
